b4dd3184f50c34b462b0ecf9e9f950754a62479a
Nguyen Cong Sang <cs.ncsang@gmail.com>
2020-01-07 21:55:21 +0700

    Lowest common multiple

---
+++ src/lowest_common_multiple.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+int GCD(int a, int b)
+{
+    int temp;
+    while (b != 0)
+    {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+};
+int main()
+{
+    int a, b;
+    std::cout << "Nhap a: ";
+    std::cin >> a;
+    std::cout << "Nhap b: ";
+    std::cin >> b;
+    int GCM = GCD(a, b);
+    std::cout << "Greatest common divisor: " << GCM << std::endl;
+    int LCM = (a * b) / GCM;
+    std::cout << "Lowest common multiple: " << LCM << std::endl;
+}
